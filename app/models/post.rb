cc155ce73c101f8a670032d1ce52ce0f9256c6b6
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-23 17:43:35 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+    validate :clickbait_y
+
+    def clickbait_y
+        return false if title.nil?
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        var = clickbait.any? do |bait|
+            title.include?(bait)
+        end
+        if var == false
+            errors.add(:title, "this shit broken")
+        end
+    end
 end
