6e2164b633352101df7b6a9d41e5039eaf26089a
cjunks94 <cjunks94@gmail.com>
2018-05-23 11:03:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true, length: {minimum: 10}
+  validates :summary, presence: true, length: {maximum: 20}
+  validates :invalid_cat, acceptance: true
+  validates :is_clickbait, acceptance: true
+
+  def invalid_cat
+    if self.category == "Fiction"
+    else
+      false
+    end
+  end
+  def is_clickbait
+    if self.title == "You Won't Believe These True Facts"
+    else
+      false
+    end
+  end
 end
