3ced808db8732a22d8542cd55e1e2cc68b77634e
travisdock <travisdock@gmail.com>
2018-05-24 06:49:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: :true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {:in => %w(Fiction Non-Fiction)}
+
+  validates_format_of :title, :with => /won't\sbelieve|secret|top\s[0-9]+|guess/i
 end
