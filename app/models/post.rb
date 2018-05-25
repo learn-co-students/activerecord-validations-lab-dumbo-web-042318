fde1c59321923de6a696fc08da4839caa1d3e736
github username <github email address>
2018-05-25 16:20:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :is_included?
+
+  private
+
+  def is_included?
+    if title
+      if !title.include? "Won't Believe" || "Secret" || "Guess" || "Top[number]"
+          errors.add(:title, "Wrong title")
+      end
+    end
+  end
+
 end
